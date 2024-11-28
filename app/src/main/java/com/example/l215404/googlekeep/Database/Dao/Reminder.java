bdb6bcb96e28a900cbdc43fcd4b18c22fcb53bba
Urwa Ihtesham <urwaihtesham@gmail.com>
2024-11-28 23:39:05 +0500

    database: Added ReminderDao

---
+++ app/src/main/java/com/example/l215404/googlekeep/Database/Dao/Reminder.java
@@ -0,0 +1,19 @@
+package com.example.l215404.googlekeep.Database.Dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.l215404.googlekeep.Database.models.User;
+
+@Dao
+public interface Reminder {
+    @Insert
+    void insertReminder(Reminder reminder);
+
+    @Query("SELECT * FROM reminders WHERE reminderId = :reminderId")
+    User getReminderByID(int reminderId);
+
+    @Query("SELECT * FROM reminders WHERE noteId = :noteId")
+    User getReminderByNoteID(int noteId);
+}
