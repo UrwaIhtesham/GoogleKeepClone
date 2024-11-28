9f63d32d8160fe13d49eb76f950a3581fdbb8464
Saba-Shafique <saba.shafique2014@gmail.com>
2024-11-29 03:51:14 +0500

    Archive and Delete notes layout

---
+++ app/src/main/java/com/example/l215404/googlekeep/ArchivePage/ArchiveActivity.java
@@ -0,0 +1,27 @@
+package com.example.l215404.googlekeep.ArchivePage;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.l215404.googlekeep.R;
+
+
+public class ArchiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_archive);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.archive_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
